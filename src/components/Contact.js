bb52565172a582998d702c01d4b2c48d47c874ba
borismassesa <bmmassesa@gmail.com>
2022-06-11 19:16:23 -0700

    Using Object destructuring

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -3,20 +3,43 @@ import React from "react"
 import phoneIcon from "../images/phone-icon.png"
 import emailIcon from "../images/mail-icon.png"
 
-export default function Contact(props){
+// export default function Contact(props){
+//   return (
+//     <div className="contacts">
+        
+//             <div className="contact-card">
+//                 <img src={props.img} />
+//                 <h3>{props.name}</h3>
+//                 <div className="info-group">
+//                     <img src={phoneIcon} />
+//                     <p>{props.phone}</p>
+//                 </div>
+//                 <div className="info-group">
+//                     <img src={emailIcon} />
+//                     <p>{props.email}</p>
+//                 </div>
+//             </div>
+//     </div>
+//   )
+// }
+
+
+// Using Destructuring method (Object destruring)
+
+export default function Contact({img,name,phone,email}){
   return (
     <div className="contacts">
         
             <div className="contact-card">
-                <img src={props.img} />
-                <h3>{props.name}</h3>
+                <img src={img} />
+                <h3>{name}</h3>
                 <div className="info-group">
                     <img src={phoneIcon} />
-                    <p>{props.phone}</p>
+                    <p>{phone}</p>
                 </div>
                 <div className="info-group">
                     <img src={emailIcon} />
-                    <p>{props.email}</p>
+                    <p>{email}</p>
                 </div>
             </div>
     </div>
